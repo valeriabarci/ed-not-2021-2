3a08ed5a5a06736520bf3b60d8481c26237327a2
Valéria Barci <73722482+valeriabarci@users.noreply.github.com>
2021-08-06 20:24:41 -0300

    (06/08) Funções e início da busca sequencial

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,7 @@
+# Importar o valor da constante pi
+#math é o nome da biblioteca onde pi se encontra
+from math import pi
+
 # Função é um trecho de código que tem um nome e pode
 # receber informações externas para fazer seu trabalho.
 # Opcionalmente, uma função pode também produzir um valor de resultado.
@@ -7,13 +11,13 @@
 # len(), range() e sort(), e podemos definir também nossas próprias funções.
 
 def imc(peso, altura): # Definição (ou declaração) da função
-"""
-    Função que calcula o Índice de Massa Corpórea (IMC)
-"""
+    """
+        Função que calcula o Índice de Massa Corpórea (IMC)
+    """
     # Trechos entre as pas triplas são um tipo especial de comentário chamado docstring,
     # e servem para documentar o propósito de uma função ou classe
 
-     return peso / altura ** 2 #peso / (altura)²
+    return peso / altura ** 2 #peso / altura²
 
 #float(): converte o valor informado em um número com casas decimais (floating point)
 p = float(input('Informe o peso da pessoa:'))
@@ -23,3 +27,27 @@ a = float(input('Informe a altura da pessoa:'))
 resultado = imc(p, a)
 
 print(f"O IMC calculado é {resultado}.")
+
+def area_forma(base, altura, forma):
+    """
+        Função que calcula a área de uma das seguintes
+        formas geométricas: retângulo, triângulo ou elipse
+        Parâmetros forma:
+        "R" == retângulo
+        "T" == triângulo
+        "E" == elipse
+    """
+    area = 0
+    if forma == "R": # Retângulo
+        area = base * altura
+    elif forma == "T": # Triângulo
+        area = base * altura / 2
+    elif forma == "E": # Elipse
+        area = (base / 2) * (altura / 2) * pi
+    return area
+
+print(f"Retângulo 7.5x11: {area_forma(7.5, 11, 'R')}")
+print(f"Triângulo 8x12: {area_forma(8, 12, 'T')}")
+print(f"Círculo 15x15: {area_forma(15, 15, 'E')}")
+print(f"Quadrado 9.5x9.5: {area_forma(9.5, 9.5)}")
+
