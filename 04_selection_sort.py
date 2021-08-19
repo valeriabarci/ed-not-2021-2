b107c5310bb234ac1a02a25ce8a7372009e949b0
Valéria Barci <73722482+valeriabarci@users.noreply.github.com>
2021-08-19 20:27:19 -0300

    (19/08) Término do bubble sort e início do selection sort

---
+++ 04_selection_sort.py
@@ -0,0 +1,9 @@
+# SLGORITMO DE ORDENAÇÃO SELECTION SORT
+#
+# Isola (seleciona) o primeiro elemento da lista e, em seguida,
+# encontra o menor valor no restante da lista. Se o valor encontrado
+# for menor que o valor selecionado, efetua a troca.
+# Em seguida, isola o segundo número da lista, buscando pelo menor
+# valor das posições subsequentes. Faz a troca entre os dois valores,
+# se necessário. Continua o processo, até isolar o penúltimo elemento
+# da lista.
